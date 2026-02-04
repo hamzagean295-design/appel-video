3b79fb8cdec80ef399876ba3b3be2d2a8d713aef
aitaddihamza <aitaddi.hamza365@gmail.com>
2026-02-04 04:49:38 +0100

    pusher isn't the problem

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -9,8 +9,4 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true,
-    wsHost: import.meta.env.VITE_PUSHER_HOST,
-    wsPort: import.meta.env.VITE_PUSHER_PORT,
-    wssPort: import.meta.env.VITE_PUSHER_PORT,
-    enabledTransports: ["ws", "wss"],
 });
